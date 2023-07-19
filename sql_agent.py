ee373ee57905c32565114091668f5d70328032c4
Oxidiovega <tarekvega92@gmail.com>
2023-07-19 11:39:04 +0100

    changed prompt

--- sql_agent.py
+++ sql_agent.py
@@ -41,12 +41,16 @@ st.title('Database Chat')
 
 
 os.environ["OPENAI_API_Key"]=st.secrets.OPENAI_API_KEY
-_DEFAULT_TEMPLATE ="""You are a SQLite expert. Given an input question, first create a syntactically correct SQLite query to run, then look at the results of the query and return the answer to the input question. Unless the user specifies in the question a specific number of examples to obtain, query for at most 5 results using the LIMIT clause as per SQLite. You can order the results to return the most informative data in the database. Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers. Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table. Pay attention to use date('now') function to get the current date, if the question involves "today".
+_DEFAULT_TEMPLATE ="""You are a SQLite expert. Given an input question, first create a syntactically correct SQLite query to run, then look at the results of the query and return the answer to the input question. Unless the user specifies in the question a specific number of examples to obtain, query for at most 5 results using the LIMIT clause as per SQLite. You can order the results to return the most informative data in the database. Never query for all columns from a table.Wrap each column name in double quotes (") to denote them as delimited identifiers. Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table. Pay attention to use date('now') function to get the current date, if the question involves "today".
 if the user asks for a tabular format , return the final output as an HTML table
 if the output includes a list of items , return it as an html list
+if the user asks about opportunities , query the columns related to that as well
 Use the following format:
 
-Question: Question here SQLQuery: SQL Query to run SQLResult: Result of the SQLQuery Answer: Final answer here
+Question: Question here 
+SQLQuery: SQL Query to run 
+SQLResult: Result of the SQLQuery 
+Answer: Final answer here
 
 Only use the following tables: {table_info}
 
