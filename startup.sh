22f877d08104c886e2464444ca7c25b59c5da790
Oxidiovega <tarekvega92@gmail.com>
2023-09-09 17:35:12 +0100

    added config command

--- startup.sh
+++ startup.sh
@@ -4,6 +4,10 @@ mkdir -p ~/.aws
 echo "[default]" > ~/.aws/credentials
 echo "aws_access_key_id = $Access_key_ID" >> ~/.aws/credentials
 echo "aws_secret_access_key = $Secret_access_key" >> ~/.aws/credentials
+
+echo "[default]" > ~/.aws/config
+echo "region = us-east-1" >> ~/.aws/config
+
 exec streamlit run insights.py --server.port 8000 --server.address 0.0.0.0
 
   
